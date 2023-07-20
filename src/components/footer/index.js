1c1439b4cc1288fdc3fa136b9244296e43b93857
Harsh Sharma <harshsharma916@gmail.com>
2023-07-20 18:21:40 +0530

    minor changes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -16,22 +16,51 @@ const Footer = () => {
           <div className={styles.follow}>
             <p>Follow us on</p>
             <div>
-              <img src={Insta} onClick={() => {window.location.href="https://www.instagram.com/the__longing/?igshid=MzRlODBiNWFlZA%3D%3D"}}/>
-              <img src={Telegram} onClick={() => {window.location.href="https://t.me/OM0111"}}/>
-              <img src={Youtube}/>
+              <img
+                src={Insta}
+                onClick={() => {
+                  window.location.href =
+                    "https://www.instagram.com/the__longing/?igshid=MzRlODBiNWFlZA%3D%3D";
+                }}
+              />
+              <img
+                src={Telegram}
+                onClick={() => {
+                  window.location.href = "https://t.me/OM0111";
+                }}
+              />
+              <img
+                onClick={() =>
+                  (window.location.href = "https://youtube.com/@kartikbansal20")
+                }
+                src={Youtube}
+              />
             </div>
           </div>
         </div>
         <div className={styles.other_links}>
           <p>Other Links</p>
-          <p> About Us</p>
-          <p>Courses</p>
+          <p
+            onClick={() =>
+              (window.location.href = "https://longing.co.in/#aboutSection")
+            }
+          >
+            {" "}
+            About Us
+          </p>
+          <p
+            onClick={() =>
+              (window.location.href = "https://longing.co.in/#coursesSection")
+            }
+          >
+            Courses
+          </p>
         </div>
         <div className={styles.contact_us}>
           <p>Contact Us</p>
           <div className={styles.email}>
             <img src={Email} />
-            <p>kartik@longing.co.in</p>
+            <a href="mailto: kartik@longing.co.in">kartik@longing.co.in</a>
           </div>
         </div>
         {!isDesktopOrTablet && (
