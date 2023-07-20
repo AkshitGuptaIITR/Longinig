267f8e1641cfa0584e96437f06730beb53ad378d
Harsh Sharma <harshsharma916@gmail.com>
2023-07-20 18:13:16 +0530

    minor changes

---
+++ src/components/footer/index.js
@@ -0,0 +1,52 @@
+import Logo from "../../assets/company/mobile_logo.svg";
+import Insta from "../../assets/icons/insta.svg";
+import Telegram from "../../assets/icons/telegram.svg";
+import Youtube from "../../assets/icons/youtube.svg";
+import Email from "../../assets/icons/email.svg";
+import styles from "./footer.module.scss";
+import { useMediaQuery } from "react-responsive";
+
+const Footer = () => {
+  const isDesktopOrTablet = useMediaQuery({ minWidth: 768 });
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footer_cont}>
+        <div>
+          <img src={Logo} className={styles.logo} />
+          <div className={styles.follow}>
+            <p>Follow us on</p>
+            <div>
+              <img src={Insta} onClick={() => {window.location.href="https://www.instagram.com/the__longing/?igshid=MzRlODBiNWFlZA%3D%3D"}}/>
+              <img src={Telegram} onClick={() => {window.location.href="https://t.me/OM0111"}}/>
+              <img src={Youtube}/>
+            </div>
+          </div>
+        </div>
+        <div className={styles.other_links}>
+          <p>Other Links</p>
+          <p> About Us</p>
+          <p>Courses</p>
+        </div>
+        <div className={styles.contact_us}>
+          <p>Contact Us</p>
+          <div className={styles.email}>
+            <img src={Email} />
+            <p>kartik@longing.co.in</p>
+          </div>
+        </div>
+        {!isDesktopOrTablet && (
+          <div className={styles.rights}>
+            <p>2023, Longing, All rights reserved</p>
+          </div>
+        )}
+      </div>
+      {isDesktopOrTablet && (
+        <div className={styles.rights}>
+          <p>2023, Longing, All rights reserved</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Footer;
